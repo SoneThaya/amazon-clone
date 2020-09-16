cc79b0d5d9e3ee6cc6477e673a2283e70a61fd87
Sone <sthaya@gmail.com>
2020-09-15 18:14:30 -0700

    started amazon clone project, added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1LXV6EO0vqrOW48dN3g1p78USkj_akIA",
+  authDomain: "challenge-11eee.firebaseapp.com",
+  databaseURL: "https://challenge-11eee.firebaseio.com",
+  projectId: "challenge-11eee",
+  storageBucket: "challenge-11eee.appspot.com",
+  messagingSenderId: "494962345970",
+  appId: "1:494962345970:web:06d4e341340d3878450d6b",
+  measurementId: "G-ELL0T7Q654",
+};
