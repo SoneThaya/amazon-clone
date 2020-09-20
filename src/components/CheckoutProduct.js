3181e0a1e442fd576af8cd4165c9ba3123a92ef5
Sone <sthaya@gmail.com>
2020-09-20 11:57:24 -0700

    coded checkout product component functionality displays in checkout

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import './CheckoutProduct.css'
+import React from "react";
+import "./CheckoutProduct.css";
 
-const CheckoutProduct = () => {
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
   return (
     <div className="checkoutProduct">
-      
-    </div>
-  )
-}
+      <img className="checkoutProduct__image" src={image} alt="product" />
 
-export default CheckoutProduct
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
+        </div>
+        <button>Remove from Basket</button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProduct;
