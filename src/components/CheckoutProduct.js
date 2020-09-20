fc6a15a6f95c55fef364f1a1d5fb45c4bbf91cf0
Sone <sthaya@gmail.com>
2020-09-20 12:49:53 -0700

    able to remove item from cart

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 const CheckoutProduct = ({ id, image, title, price, rating }) => {
+
+  const [{basket}, dispatch] = useStateValue()
+
+  const removeFromBasket = () => {
+    dispatch({
+      type: 'REMOVE_FROM_BASKET',
+      id: id,
+    })
+  }
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="product" />
@@ -20,7 +31,7 @@ const CheckoutProduct = ({ id, image, title, price, rating }) => {
               <p>⭐️</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
